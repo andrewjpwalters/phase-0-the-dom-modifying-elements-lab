a039d1c3f78b27bab012c169cf7b6de77b9044a8
Andrew Walters <andrewjpwalters@gmail.com>
2022-04-02 02:31:29 -0500

    commit tree 248b6e2ddee8dc73045b9cd9230b43f2542ae774

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+const main = document.querySelector('#main');
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+newHeader.id = 'victory';
+
+document.body.append(newHeader);
